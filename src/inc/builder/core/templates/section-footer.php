dd990e7f739e4c7ffdfb72001b8aff2fd0f026ff
sixFingers <ignazio.setti@gmail.com>
2017-09-02 17:08:16 +0200

    Remove copy of shared code

--- src/inc/builder/core/templates/section-footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package Make
- */
-?>
-
-	<textarea name="ttfmake-section-json-{{ data.get('id') }}" class="ttfmake-section-json" style="display: none;">{{ JSON.stringify( data.toJSON() ) }}</textarea>
-</div>
--- src/inc/sections/setupinterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @package Make
- */
-
-
-interface MAKE_Sections_SetupInterface extends MAKE_Util_ModulesInterface {}
